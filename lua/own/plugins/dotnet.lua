9e3af7ba030f4846c28bc0a5031c9955c244d1f6
Simeon Iksanov <simeon.iksanov@gmail.com>
2025-09-05 11:03:38 +0500

    add call for diagnostics on insert leave

--- lua/own/plugins/dotnet.lua
+++ lua/own/plugins/dotnet.lua
@@ -1,3 +1,23 @@
+-- got it here https://github.com/sebastianstudniczek/.dotfiles/blob/main/nvim/.config/nvim/lua/plugins/roslyn.lua
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  pattern = "*",
+  callback = function()
+    local clients = vim.lsp.get_clients({ name = "roslyn" })
+    if not clients or #clients == 0 then
+      return
+    end
+
+    local client = assert(clients[1])
+    local buffers = vim.lsp.get_buffers_by_client_id(client.id)
+    for _, buf in ipairs(buffers) do
+      local params = {
+        textDocument = vim.lsp.util.make_text_document_params(buf),
+      }
+      client:request(vim.lsp.protocol.Methods.textDocument_diagnostic, params, nil, buf)
+    end
+  end,
+})
+
 return {
   {
     "seblyng/roslyn.nvim",
